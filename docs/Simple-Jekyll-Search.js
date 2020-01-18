788cc8af75143187f3b998da98f16bc1b957146a
Barry Schneider <bschneider@msih.com>
2020-01-18 16:03:16 -0500

    fix ltolowercase

--- docs/Simple-Jekyll-Search.js
+++ docs/Simple-Jekyll-Search.js
@@ -118,9 +118,8 @@
       if (!str) return false
 
       str = str.trim().toLowerCase()
-      crit = crit.trim().toLowerCase()
 
-      return crit.split(' ').filter(function (word) {
+      return crit.split(' ').trim().toLowerCase().filter(function (word) {
         return str.indexOf(word) >= 0
       }).length === crit.split(' ').length
     }
