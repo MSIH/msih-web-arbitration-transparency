5a63a71c1c5340afa550368b1887d274c4e525bc
Barry Schneider <bschneider@msih.com>
2020-01-18 15:57:32 -0500

    fix forEach

--- docs/Simple-Jekyll-Search.js
+++ docs/Simple-Jekyll-Search.js
@@ -223,8 +223,8 @@
     var categories = crit.match(/\(.*?\)/g); //find strings within ()
     console.log("tags: " + tags)
     console.log("categories: " + categories)
-    var contents = tags ? tags.foreach(element => crit.replace(element, "")) : null
-    contents = categories ? categories.foreach(element => contents.replace(element, "")) : null
+    var contents = tags ? tags.forEach(element => crit.replace(element, "")) : null
+    contents = categories ? categories.forEach(element => contents.replace(element, "")) : null
 
     console.log("contents: " + contents)
 
